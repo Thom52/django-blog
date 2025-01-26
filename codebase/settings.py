cde972004287cd95f07d9f214092b9bdca114ec0
Thom Hunkin <thom.hunkin@gmail.com>
2025-01-26 18:25:12 +0000

    Add files to deply static content to heroku

--- codebase/settings.py
+++ codebase/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-7z-&+4#3@_9$k+x)l&bs4)1vx^_9+chdvtm%m6vg7m75))porg
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-thom52-djangoblog-6sdn6rzqvae.ws-eu117.gitpod.io', '.herokuapp.com']
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,6 +136,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
